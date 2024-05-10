b0b4da1ebfae3a04e4e967d0ac132184c3ce957d
Supernova-428 <yosung22@gmail.com>
2024-05-10 16:34:02 +0900

    내부 컴포넌트들 생성

---
+++ react-study/chapter1/day1/src/components/ExpenseList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './ExpenseList.css';
+
+export default class ExpenseList extends Component {
+  render() {
+    return (
+        <>
+            <ul className='list'>
+
+            </ul>
+            <button className='btn'>
+                목록지우기
+            </button>
+        </>
+    )
+  }
+}
