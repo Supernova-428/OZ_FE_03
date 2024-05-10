0c008904304c8964678957a46416104267367529
Supernova-428 <yosung22@gmail.com>
2024-05-10 17:05:21 +0900

    Item 컴포넌트 생성

--- react-study/chapter1/day1/src/components/ExpenseList.js
+++ react-study/chapter1/day1/src/components/ExpenseList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './ExpenseList.css';
+import ExpenseItem from './ExpenseItem';
 
 export default class ExpenseList extends Component {
   render() {
     return (
         <>
             <ul className='list'>
-
+                <ExpenseItem />
             </ul>
             <button className='btn'>
                 목록지우기
