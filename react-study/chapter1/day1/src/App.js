9dc50cc9c74bda0513a2645590bf256d1748a705
Supernova-428 <yosung22@gmail.com>
2024-05-13 17:03:05 +0900

    react state

--- react-study/chapter1/day1/src/App.js
+++ react-study/chapter1/day1/src/App.js
@@ -5,14 +5,20 @@ import ExpenseList from './components/ExpenseList';
 
 export default class App extends Component {
 
-  initialExpenses = [
-    {id: 1, charge: '콜라', amount: 2000},
-    {id: 2, charge: '빵', amount: 1000},
-    {id: 3, charge: '맥북', amount: 20000},
-  ]
+  constructor(props){
+    super(props);
+    this.state = {
+      expenses: [
+        {id: 1, charge: '콜라', amount: 2000},
+        {id: 2, charge: '빵', amount: 1000},
+        {id: 3, charge: '맥북', amount: 20000},
+      ]
+    }
+  }
 
   handleDelete = (id) => {
-    const newExpense = this.initialExpenses.filter(expense => expense.id !== id)
+    const newExpense = this.state.expenses.filter(expense => expense.id !== id)
+    this.setState({expenses: newExpense})
   }
 
   render(){
@@ -26,7 +32,7 @@ export default class App extends Component {
           </div>
           <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
             {/* {Expense List} */}
-            <ExpenseList initialExpenses={this.initialExpenses} handleDelete={this.handleDelete}/>
+            <ExpenseList initialExpenses={this.state.expenses} handleDelete={this.handleDelete}/>
           </div>
           <div style={{display: 'flex', justifyContent: 'start', marginTop: '1rem'}}>
             <p style={{fontSize: '2rem'}}>
