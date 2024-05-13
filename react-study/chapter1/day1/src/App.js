b6ebbca35b2e5def957f790015184a155116a33b
Supernova-428 <yosung22@gmail.com>
2024-05-13 18:30:25 +0900

    클래스 -> 함수형 컴포넌트로 바꾸기

--- react-study/chapter1/day1/src/App.js
+++ react-study/chapter1/day1/src/App.js
@@ -1,46 +1,41 @@
-import {Component} from 'react';
+import {Component, useState} from 'react';
 import './App.css';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 
-export default class App extends Component {
+const App = () => {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      expenses: [
-        {id: 1, charge: '콜라', amount: 2000},
-        {id: 2, charge: '빵', amount: 1000},
-        {id: 3, charge: '맥북', amount: 20000},
-      ]
-    }
+  const [expenses, setExpenses] = useState([
+    {id: 1, charge: '콜라', amount: 2000},
+    {id: 2, charge: '빵', amount: 1000},
+    {id: 3, charge: '맥북', amount: 20000},
+  ])
+
+  const handleDelete = (id) => {
+    const newExpense = expenses.filter(expense => expense.id !== id)
+    setExpenses(newExpense)
   }
 
-  handleDelete = (id) => {
-    const newExpense = this.state.expenses.filter(expense => expense.id !== id)
-    this.setState({expenses: newExpense})
-  }
-
-  render(){
-    return(
-      <main className='main-container'>
-        <div className='sub-container'>
-          <h1>장바구니</h1>
-          <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
-            {/* {Expense Form} */}
-            <ExpenseForm />
-          </div>
-          <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
-            {/* {Expense List} */}
-            <ExpenseList initialExpenses={this.state.expenses} handleDelete={this.handleDelete}/>
-          </div>
-          <div style={{display: 'flex', justifyContent: 'start', marginTop: '1rem'}}>
-            <p style={{fontSize: '2rem'}}>
-              총 합계:
-            </p>
-          </div>
+  return(
+    <main className='main-container'>
+      <div className='sub-container'>
+        <h1>장바구니</h1>
+        <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
+          {/* {Expense Form} */}
+          <ExpenseForm />
         </div>
-      </main>
-    )
-  }
+        <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
+          {/* {Expense List} */}
+          <ExpenseList initialExpenses={expenses} handleDelete={handleDelete}/>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'start', marginTop: '1rem'}}>
+          <p style={{fontSize: '2rem'}}>
+            총 합계:
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
+
+export default App;
