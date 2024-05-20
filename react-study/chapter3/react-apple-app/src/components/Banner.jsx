e2eeb996d6e276a89dcf38684503676e6cec98c7
Supernova-428 <yosung22@gmail.com>
2024-05-20 19:02:32 +0900

    이미지 배너를 위한 데이터 생성

--- react-study/chapter3/react-apple-app/src/components/Banner.jsx
+++ react-study/chapter3/react-apple-app/src/components/Banner.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import './Banner.css'
+import axios from '../api/axios'
+import Requests from '../api/requests'
 
 const Banner = () => {
+
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+      fetchData()
+
+    }, [])
+
+    const fetchData = async() => {
+        // 현재 상영중인 영화 정보를 가져오기 (여러 영화)
+        const response = await axios.get(Requests.fetchNowPlaying)
+        // 여러 영화 중 영화 하나의 ID를 가져오기
+        const movieId = response.data.results[
+            Math.floor(Math.random() * response.data.results.length)
+        ].id
+        // 특정 영화의 상세정보 가져오기
+        const {data: movieDetail} = await axios.get(`movie/${movieId}`, {
+            params: {append_to_response: 'videos'}
+        })
+        setMovie(movieDetail)
+    }
+
   return (
     <div>Banner</div>
   )
