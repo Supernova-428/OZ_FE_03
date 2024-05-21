3fbb729cca8afdff3d4135cbf1203295dbdec75b
Supernova-428 <yosung22@gmail.com>
2024-05-21 16:59:44 +0900

    비디오 배너 생성

--- react-study/chapter3/react-apple-app/src/components/Banner.jsx
+++ react-study/chapter3/react-apple-app/src/components/Banner.jsx
@@ -2,10 +2,12 @@ import React, { useEffect,useState } from 'react'
 import './Banner.css'
 import axios from '../api/axios'
 import Requests from '../api/requests'
+import styled from "styled-components";
 
 const Banner = () => {
 
     const [movie, setMovie] = useState(null)
+    const [isClicked, setIsClicked] = useState(false)
 
     useEffect(() => {
       fetchData()
@@ -36,7 +38,8 @@ const Banner = () => {
                 loading...
             </div>
           )
-    }else{
+    }
+    if (!isClicked) {
         return (
           <div className='banner'
           style={{
@@ -50,7 +53,7 @@ const Banner = () => {
                 </h1>
                 <div className='banner_buttons'>
                     {movie.videos?.results[0]?.key ?
-                        <button className='banner_button play'>
+                        <button className='banner_button play' onClick={() => setIsClicked(true)}>
                             play
                         </button>
                         : null
@@ -60,10 +63,44 @@ const Banner = () => {
                     {truncate(movie.overview, 100)}
                 </p>
             </div>
+            <div className='banner_fadeBottom'/>
           </div>
+        )    
+    }else{
+        return(
+            <>
+            <Container>
+                <HomeContainer>
+                    <Iframe src={`https://www.youtube.com/embed/${movie.videos.results[0].key}`}></Iframe>
+                </HomeContainer>
+            </Container>
+            <button onClick={() => setIsClicked(false)}>
+                X
+            </button>
+            </>
         )
     }
-
 }
+
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0.65;
+    border: none;
+`
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+`
+const HomeContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`
 
 export default Banner
