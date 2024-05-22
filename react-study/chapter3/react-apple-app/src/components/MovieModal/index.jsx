ba710d5a7e078f97c216ddab7be5dbac24eb09ad
Supernova-428 <yosung22@gmail.com>
2024-05-22 19:54:13 +0900

    movie 모달 UI 생성

--- react-study/chapter3/react-apple-app/src/components/MovieModal/index.jsx
+++ react-study/chapter3/react-apple-app/src/components/MovieModal/index.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { imageBasePath } from '../../constant'
 
-const MovieModal = ({backdrop_path, title, overView, name, release_date, first_air_date, vote_average, setModalOpen}) => {
+const MovieModal = ({backdrop_path, title, overview, name, release_date, first_air_date, vote_average, setModalOpen}) => {
   return (
-    <div>MovieModal</div>
+    <div className='presentation' role='presentation'>
+        <div className='wrapper-modal'>
+            <div className='modal'>
+                <span onClick={() => setModalOpen(false)} className='modal-close'>
+                    X
+                </span>
+                <img className='modal_poster-img' src={`${imageBasePath}${backdrop_path}`} />
+                <p className='modal_details'>
+                    <span>
+                        100% for you
+                    </span>{' '}
+                    {release_date ? release_date : first_air_date}
+                </p>
+                <h2 className='modal_title'>
+                    {title ? title : name}
+                </h2>
+                <p className='modal_overview'>
+                    평점: {vote_average}
+                </p>
+                <p className='modal_overview'>
+                    {overview}
+                </p>
+            </div>
+        </div>
+    </div>
   )
 }
 
