04bab10a05873b04abd6d757cb2ad44301d305f7
Supernova-428 <yosung22@gmail.com>
2024-05-22 17:24:25 +0900

    Row 컴포넌트 생성

--- react-study/chapter3/react-apple-app/src/components/Row.jsx
+++ react-study/chapter3/react-apple-app/src/components/Row.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import './Row.css'
+import axios from '../api/axios'
 
-const Row = () => {
+const Row = ({title, id, fetchUrl}) => {
+
+  const [movies, setMovies] = useState([])
+
+  const fetchMovieData = useCallback(async () => {
+    const response = await axios.get(fetchUrl)
+    setMovies(response.data.results)
+  },[fetchUrl])
+
+  useEffect(() => {
+    fetchMovieData()
+  }, [fetchMovieData])
+
   return (
-    <div>Row</div>
+    <div>
+      <h2>{title}</h2>
+      <div className='slider'>
+        <div className='slider_arrow-left'>
+          <span className='arrow'>
+            {'<'}
+          </span>
+        </div>
+        <div id={id} className='row_posters'>
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className='row_poster'
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className='slider_arrow-right'>
+          <span className='arrow'>
+            {'>'}
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
