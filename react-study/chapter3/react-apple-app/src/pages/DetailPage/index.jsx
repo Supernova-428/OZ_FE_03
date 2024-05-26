c504b5961bf76b3357f0e794eed1f62316fc0425
Supernova-428 <yosung22@gmail.com>
2024-05-26 20:14:24 +0900

    리액트 9일차 과제

--- react-study/chapter3/react-apple-app/src/pages/DetailPage/index.jsx
+++ react-study/chapter3/react-apple-app/src/pages/DetailPage/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 import axios from '../../api/axios'
 import {imageBasePath} from '../../constant'
+import './DetailPage.css'
 
 const DetailPage = () => {
 
@@ -15,12 +16,33 @@ const DetailPage = () => {
     }
     fetchData()
   }, [movieId])
+  console.log(movie)
   
   if (!movie) return null
 
   return (
-    <section>
-      <img src={`${imageBasePath}${movie.backdrop_path}`} alt='detail'/>
+    <section className='detail_section'>
+      <img className='detail_img' src={`${imageBasePath}${movie.backdrop_path}`} alt='detail'/>
+      <h1 className='detail_title'>
+        {movie.title ? movie.title : movie.name}
+      </h1>
+      <p className='detail_voteAverage'>
+        평점: {movie.vote_average}
+      </p>
+      <h3>
+        장르
+      </h3>
+      <p className='detail_genres'>
+        {movie.genres.map((genre) => {
+          return <span className='detail_genre' key={genre.id}>{genre.name}</span>
+        })}
+      </p>
+      <h3>
+        줄거리
+      </h3>
+      <p className='detail_overview'>
+        {movie.overview}
+      </p>
     </section>
   )
 }
