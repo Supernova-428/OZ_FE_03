3106c1941c67490a331a2879f6a9501d6108443b
Supernova-428 <yosung22@gmail.com>
2024-05-27 17:49:15 +0900

    firebase 연결

---
+++ react-study/chapter3/react-apple-app/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjo8IBZ8SSlhS6SanIuB-qDqNnfXOy5qg",
+  authDomain: "react-apple-app-2b52b.firebaseapp.com",
+  projectId: "react-apple-app-2b52b",
+  storageBucket: "react-apple-app-2b52b.appspot.com",
+  messagingSenderId: "998072774020",
+  appId: "1:998072774020:web:f5e1491ab7ecc3486481d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
