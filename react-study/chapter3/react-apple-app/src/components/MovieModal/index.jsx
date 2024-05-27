54d6fe99f04564f9fcb77892d4a132577af73efc
Supernova-428 <yosung22@gmail.com>
2024-05-27 17:24:26 +0900

    useOnClickOutside Hooks

--- react-study/chapter3/react-apple-app/src/components/MovieModal/index.jsx
+++ react-study/chapter3/react-apple-app/src/components/MovieModal/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { imageBasePath } from '../../constant'
 import './MovieModal.css'
+import useOnClickOutside from '../../hooks/useOnClickOutside'
 
 const MovieModal = ({backdrop_path, title, overview, name, release_date, first_air_date, vote_average, setModalOpen}) => {
-  return (
+    
+  const ref = useRef(null)
+  useOnClickOutside(ref, () => {
+    setModalOpen(false)
+  })
+
+  return (  
     <div className='presentation' role='presentation'>
         <div className='wrapper-modal'>
-            <div className='modal'>
+            <div className='modal' ref={ref}>
                 <span onClick={() => setModalOpen(false)} className='modal-close'>
                     X
                 </span>
