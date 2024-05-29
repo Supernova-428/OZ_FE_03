ca7afe755c0cc9cf9ed5563c652b9b8542f58a62
Supernova-428 <yosung22@gmail.com>
2024-05-29 16:53:11 +0900

    ExpenseList 컴포넌트 생성

--- react-study/chapter5/react-budget-app/src/index.js
+++ react-study/chapter5/react-budget-app/src/index.js
@@ -8,7 +8,7 @@ import { AppContextProvider } from './contexts/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AppContextProvider value={{}}>
+  <AppContextProvider>
     <App />
   </AppContextProvider>
 );
