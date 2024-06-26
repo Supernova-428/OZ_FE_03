d0289b8ac6419297be462b317896c54504e24cbd
Supernova-428 <yosung22@gmail.com>
2024-06-26 20:28:53 +0900

    TS basic types

---
+++ typescript-study/basic-types/dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+// string
+const a = '';
+const b = "";
+const c = ``;
+// number
+let n = 100;
+let dot = 100.123;
+let minus = -100;
+let mul = n * dot;
+let infinity = Infinity;
+let notANumber = NaN;
+//boolean
+let isOpen = true;
+// null
+let user = null;
+// any
+let someValue;
+someValue.toString();
+someValue.false;
+someValue.toFixed();
---
+++ typescript-study/basic-types/src/index.ts
@@ -0,0 +1,32 @@
+// string
+
+const a: string = '';
+const b: string = "";
+const c: string = ``;
+
+// number
+
+let n: number = 100
+let dot: number = 100.123
+let minus: number = -100
+
+let mul: number = n * dot
+
+let infinity: number = Infinity
+let notANumber: number = NaN
+
+//boolean
+
+let isOpen: boolean = true
+
+// null
+
+let user: string | null = null
+
+// any
+
+let someValue: any
+
+someValue.toString()
+someValue.false
+someValue.toFixed()
